4354e3aae9a2819bbb577bb2b870bc954c807c06
JeanMMillet <millet.jm@live.com>
2023-07-05 12:22:38 +0200

    add api call and display data in table

--- src/pages/WorkshopManager/WorkshopManager.jsx
+++ src/pages/WorkshopManager/WorkshopManager.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllSessions } from "../../services/session";
 import "./WorkshopManager.scss";
 
+const getSessions = async () => {
+  try {
+    let result = await getAllSessions();
+    result = result.data;
+    console.log("await", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
 const WorkshopManager = () => {
+  const [sessions, setSessions] = useState([]);
+  useEffect(() => {
+    const getSessions = async () => {
+      try {
+        const result = await getAllSessions();
+        setSessions(result.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getSessions();
+  }, []);
+  useEffect(() => {
+    console.log("result", sessions);
+  }, [sessions]);
+
   return (
     <div className="workshopManager">
       <h2>Gestion des Ateliers</h2>
@@ -17,19 +44,25 @@ const WorkshopManager = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>plop</td>
-            <td>plop</td>
-            <td>plop</td>
-            <td>plop</td>
-            <td>plop</td>
-            <td>
-              <button>Détails</button>
-              <button>Supprimer</button>
-            </td>
-          </tr>
+          {sessions.map((session) => {
+            return (
+              <tr>
+                <td>{session.date}</td>
+                <td>{session.category}</td>
+                <td>{session.location}</td>
+                <td>{session.max_participants}</td>
+                <td>{session.location}</td>
+
+                <td>
+                  <button>Détails</button>
+                  <button>Supprimer</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
+      <button type="button">Ajouter un atelier</button>
     </div>
   );
 };
