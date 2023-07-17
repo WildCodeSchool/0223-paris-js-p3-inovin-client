76b86af4048b0471d716b59653c8c0e103d8be98
T0MLE <lelaurain.tom@gmail.com>
2023-07-17 10:57:33 +0200

    red inputs when its red wine, yellow inputs when its white wine

--- src/pages/Creation/Creation.jsx
+++ src/pages/Creation/Creation.jsx
@@ -1,7 +1,7 @@
 import "./Creation.scss";
 import api from "../../services/api";
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function Creation() {
@@ -15,6 +15,8 @@ function Creation() {
   });
 
   const [errorMessage, setErrorMessage] = useState(null);
+
+  const nav = useNavigate();
 
   const handleChange = (e) => {
     const sum = +inputValue.wine1 + +inputValue.wine2 + +inputValue.wine3;
@@ -37,9 +39,9 @@ function Creation() {
       return;
     }
     try {
+      nav("/profilepage");
       await api.post(`recipes/${id}`);
       const result = await api.get(`recipes/session/${id}`);
-      console.log(result.data[0].id);
       await api.post("creations/", [
         {
           recipeId: result.data[0].id,
@@ -69,16 +71,25 @@ function Creation() {
   useEffect(() => {
     const groupedData = new Map();
     notes.forEach((item) => {
-      const { wine, note, tag, wine_id } = item;
+      const { wine, note, tag, wine_id, category } = item;
       if (groupedData.has(wine)) {
-        groupedData.get(wine).tags.push(tag);
+        groupedData.get(wine).tags.push({ category: category, tag: tag });
       } else {
-        groupedData.set(wine, { wine, wine_id, note, tags: [tag] });
+        groupedData.set(wine, {
+          wine,
+          wine_id,
+          note,
+          tags: [{ category: category, tag: tag }],
+        });
       }
     });
 
     setCondensedData(Array.from(groupedData.values()).slice(0, 3));
   }, [notes]);
+
+  useEffect(() => {
+    console.log(notes);
+  }, [condensedData]);
 
   return (
     <div className="creation">
@@ -100,7 +111,11 @@ function Creation() {
       <div className="inputs">
         <div className="input-container">
           <input
-            className="range-creation"
+            className={
+              notes[0]?.color == "Rouge"
+                ? "range-creation range-creation-red"
+                : "range-creation range-creation-yellow"
+            }
             type="range"
             min="0"
             max="100"
@@ -113,13 +128,19 @@ function Creation() {
             className="input-bg"
             style={{
               width: inputValue.wine1 + "%",
+              backgroundColor:
+                notes[0]?.color == "Rouge" ? "#C95B5B" : "#EBD68C",
             }}
           ></div>
         </div>
 
         <div className="input-container">
           <input
-            className="range-creation"
+            className={
+              notes[0]?.color == "Rouge"
+                ? "range-creation range-creation-red"
+                : "range-creation range-creation-yellow"
+            }
             type="range"
             min="0"
             max="100"
@@ -132,12 +153,18 @@ function Creation() {
             className="input-bg"
             style={{
               width: inputValue.wine2 + "%",
+              backgroundColor:
+                notes[0]?.color == "Rouge" ? "#C95B5B" : "#EBD68C",
             }}
           ></div>
         </div>
         <div className="input-container">
           <input
-            className="range-creation"
+            className={
+              notes[0]?.color == "Rouge"
+                ? "range-creation range-creation-red"
+                : "range-creation range-creation-yellow"
+            }
             type="range"
             min="0"
             max="100"
@@ -150,6 +177,8 @@ function Creation() {
             className="input-bg"
             style={{
               width: inputValue.wine3 + "%",
+              backgroundColor:
+                notes[0]?.color == "Rouge" ? "#C95B5B" : "#EBD68C",
             }}
           ></div>
         </div>
@@ -168,8 +197,10 @@ function Creation() {
           <p>sur 75cl</p>
         </div>
       </div>
-      <p className="error" style={{ color: errorMessage ? 'red' : 'white' }}>Le total des vins ne fait pas 100%</p>
-      <button onClick={handleClick}>créer la recette</button>
+      <p className="error" style={{ color: errorMessage ? "red" : "white" }}>
+        Le total des vins doit faire 100%
+      </p>
+      <button onClick={handleClick} className={notes[0]?.color == "Rouge" ? "btn btn-red" : "btn btn-yellow"}>Valider la recette</button>
     </div>
   );
 }
