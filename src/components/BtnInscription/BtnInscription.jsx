a36853d439d940e71583de8b5be3d7bcb9fcbd35
Brian-boudrioux <135231975+Brian-boudrioux@users.noreply.github.com>
2023-07-18 14:41:12 +0200

    Revert "Confirm message"

--- src/components/BtnInscription/BtnInscription.jsx
+++
@@ -1,9 +0,0 @@
-import "./BtnInscription.scss";
-
-import React from "react";
-
-function BtnInscription({ type }) {
-  return <button>S'inscrire à un atelier {type}</button>;
-}
-
-export default BtnInscription;
