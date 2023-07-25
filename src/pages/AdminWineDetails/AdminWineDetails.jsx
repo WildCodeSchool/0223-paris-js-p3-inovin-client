bb9a94ead30f6413e9ff3c5a0e32c898699760f3
JeanMMillet <millet.jm@live.com>
2023-07-25 17:30:10 +0200

    start admin wine details page

--- src/pages/AdminWineDetails/AdminWineDetails.jsx
+++ src/pages/AdminWineDetails/AdminWineDetails.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getWineById } from "../../services/wines";
 
 import "./AdminWineDetails.scss";
 
@@ -11,8 +12,9 @@ const AdminWineDetails = () => {
   useEffect(() => {
     const getWineInfos = async (id) => {
       try {
-        const wineInfos = await getWinesById(id);
-        setWine(wineInfos.data);
+        const wineInfos = await getWineById(id);
+        setWine(wineInfos.data[0]);
+        console.log(wineInfos.data[0]);
       } catch (error) {
         console.error(error);
       }
@@ -20,7 +22,61 @@ const AdminWineDetails = () => {
 
     getWineInfos(id);
   }, []);
-  return <div className="admin-wine-details">AdminWineDetails</div>;
+
+  const handleChange = () => {};
+
+  return (
+    <div className="admin-wine-details">
+      <div className="image">
+        <img src={wine?.image} alt="Bouteille de vin" />
+      </div>
+      <div className="content">
+        <div className="content-body">
+          <label htmlFor="">
+            Nom du vin :
+            <input
+              type="text"
+              onChange={(e) => handleChange(e)}
+              value={wine.name}
+            />
+          </label>
+          <label htmlFor="">
+            Nom du domaine :
+            <input
+              type="text"
+              onChange={(e) => handleChange(e)}
+              value={wine.domain}
+            />
+          </label>
+          <label htmlFor="">
+            Année de production :
+            <input
+              type="text"
+              onChange={(e) => handleChange(e)}
+              value={wine.manufacture_year}
+            />
+          </label>
+          <label htmlFor="">
+            Région :
+            <input
+              type="text"
+              onChange={(e) => handleChange(e)}
+              value={wine.region}
+            />
+          </label>
+          <label htmlFor="">
+            Année de production :
+            <input
+              type="text"
+              onChange={(e) => handleChange(e)}
+              value={wine.manufacture_year}
+            />
+          </label>
+        </div>
+        <div className="content-footer"></div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminWineDetails;
--- src/services/wines.js
+++ src/services/wines.js
@@ -4,8 +4,12 @@ const getAllWines = () => {
   return api.get("/wines/");
 };
 
+const getWineById = (id) => {
+  return api.get(`/wines/${id}`);
+};
+
 const deleteWine = (id) => {
   return api.delete(`/wines/${id}`);
 };
 
-export { getAllWines, deleteWine };
+export { getAllWines, deleteWine, getWineById };
